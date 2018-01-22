91916d071766702aa12d316360456b451a733ad1
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-01-22 21:53:35 -0200

    feat: My first redux application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import counter from './reducers/counter';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter);
+
+const initialRender = () => {
+  document.body.innerText = store.getState();
+};
+
+store.subscribe(initialRender);
+initialRender();
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
