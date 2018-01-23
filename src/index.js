7b867e2a8f31f9dd94c5a1cf842c0c71500c9586
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-01-22 23:46:09 -0200

    feat: Implementing Store from scratch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import counter from './reducers/counter';
+
+const createStore = (reducer) => {
+  let state;
+  let listeners = [];
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter(l => l !== listener);
+    };
+  };
+
+  dispatch({});
+
+  return { getState, dispatch, subscribe };
+};
 
 const store = createStore(counter);
 
