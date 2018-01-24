1f9a5987e77a208c79ed7e8b34953f290ca4abb4
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-01-23 22:57:40 -0200

    feat: Reducer composition with objects

--- src/index.js
+++ src/index.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import { createStore } from 'redux';
-import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import counter from './reducers/counter';
-import Counter from './components/Counter';
-import { increment, decrement } from './actions/actions';
+import todoApp from './reducers/todoApp';
 
-const store = createStore(counter);
+const store = createStore(todoApp);
 
-const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => {
-      store.dispatch(increment());
-    }}
-    onDecrement={() => store.dispatch(decrement())}
-  />,
-  document.getElementById('root'),
-);
+console.log('Initial state:');
+console.log(store.getState());
+console.log('--------------');
 
-store.subscribe(render);
-render();
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux',
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
 
-registerServiceWorker();
+console.log('Dispatching ADD_TODO.');
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping',
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching TOGGLE_TODO.');
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0,
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
+
+console.log('Dispatching SET_VISIBILITY_FILTER.');
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED',
+});
+console.log('Current state:');
+console.log(store.getState());
+console.log('--------------');
