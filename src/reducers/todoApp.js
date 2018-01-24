22caab9688ce920aaa644c480b66ce834463aa66
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-01-23 23:19:36 -0200

    feat: Reducer composition with combineReducers()

--- src/reducers/todoApp.js
+++ src/reducers/todoApp.js
@@ -1,11 +1,13 @@
+import { combineReducers } from 'redux';
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
+
 // todoApp here is the top-level reducer. which is a function that
 // accepts state and an action
 // and returns an object with the new state.
-const todoApp = (state = {}, action) => ({
-  todos: todos(state.todos, action),
-  visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+const todoApp = combineReducers({
+  todos,
+  visibilityFilter,
 });
 
 export default todoApp;
