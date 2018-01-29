b6a1eda4a4c19634ad2f69d16fba1f34e257ec2f
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2018-01-29 20:20:04 -0200

    refactor: Remove localstorage get/persist state

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,4 @@
 import { createStore } from 'redux';
-import throttle from 'lodash/throttle';
-import { loadState, saveState } from './localStorage';
 import todoApp from './reducers/index';
 
 const addLoggingToDispatch = (store) => {
@@ -23,18 +21,11 @@ const addLoggingToDispatch = (store) => {
 };
 
 const configureStore = () => {
-  const persistedState = loadState();
-  const store = createStore(todoApp, persistedState);
+  const store = createStore(todoApp);
 
   if (process.env.NODE_ENV !== 'production') {
     store.dispatch = addLoggingToDispatch(store);
   }
-
-  store.subscribe(throttle(() => {
-    saveState({
-      todos: store.getState().todos,
-    });
-  }, 1000));
 
   return store;
 };
