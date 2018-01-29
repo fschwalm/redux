0957b4113934329cbe26708980486090fdaed452
Fagner <fagnerschwalm@gmail.com>
2018-01-29 16:22:26 -0200

    refactor: Create a Root component (App entry point)

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import reactDom from 'react-dom';
-import throttle from 'lodash/throttle';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import './index.css';
-import todoApp from './reducers/todoApp';
-import TodoApp from './components/TodoApp';
-import { loadState, saveState } from './localStorage';
+import configureStore from './configureStore';
+import Root from './components/Root';
 
+const store = configureStore();
 
-const persistedState = loadState();
-const store = createStore(todoApp, persistedState);
-
-store.subscribe(throttle(() => {
-  saveState({
-    todos: store.getState().todos,
-  });
-}, 1000));
-
-reactDom.render(
-  <Provider store={store}>
-    <TodoApp />
-  </Provider>,
-  document.getElementById('root'),
-);
+reactDom.render(<Root store={store} />, document.getElementById('root'));
